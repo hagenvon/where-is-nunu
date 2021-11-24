328ad76b4289a40ac7b6b55d1aac17d1bce60432
hagenvon <31388529+hagenvon@users.noreply.github.com>
2021-11-24 20:51:19 +0100

    faqs updated

--- src/config/_faqs.ts
+++ src/config/_faqs.ts
@@ -4,7 +4,7 @@ import {AMOUNT_MODELS, MINT_DATE, PRICE, THUGS_PER_MODEL} from "./__numbers";
 export const faqs: FaqModel[] = [
     {
         question: "How many?",
-        answer: `A total supply of 1111. 60% Charlies and 40% Bubos (girls are rare).`
+        answer: `A total supply of 1111. 55% Charlies and 45% Bubos (girls are rare).`
     },
     {
         question: "How much?",
